4a5c3ca6eadfef01f11e8f883a8b146d48f03998
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-03-14 11:52:35 +0530

    Binary Search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -6,13 +6,17 @@ public class BinarySearch {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        System.out.println("Enter the  element of Array");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println("Enter the item");
         int item = sc.nextInt();
         int found=binarySearch(arr,item);
-        System.out.println(found);
+        if(found>0)
+        System.out.println("Position of item "+found);
+        else
+            System.out.println("Item Not Found");
     }
         public static  int binarySearch(int[] arr,int item){
 //            System.out.println("Enter item");
@@ -20,7 +24,7 @@ public class BinarySearch {
             while (first<=last) {
                 int mid = (first + last) / 2;
                 if (item == arr[mid])
-                    return item;
+                    return mid;
                 else if (item>arr[mid])
                     first=mid+1;
                 else
