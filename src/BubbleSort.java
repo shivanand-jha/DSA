13d2513ea09491b00d741ac92dcbe69e07bed25b
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-03-14 13:07:29 +0530

    Searching Algorithm

---
+++ src/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size");
+        int n= sc.nextInt();
+        System.out.println("Enter the element of array");
+        int[] arr=new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        BubbleSort b= new BubbleSort();
+        b.Sort(arr);
+
+    }
+
+    public void Sort(int[] arr) {
+
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j]>arr[j + 1]) {
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int z = 0; z < arr.length; z++) {
+            System.out.println(arr[z]+" ");
+        }
+    }
+}
---
+++ src/LinearSearch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int[] arr;
+        int item,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Size of Array");
+        n = sc.nextInt();
+        arr= new int[n];
+        System.out.println("Enter element of Array");
+        for (int i = 0; i < n; i++) {
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("Enter the item to be searched");
+        item=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            if(arr[i]==item){
+                System.out.println("element "+item+" is at "+i+1);
+                break;
+            }
+            else {
+                System.out.println("item not found");
+                break;
+            }
+        }
+
+    }
+}
